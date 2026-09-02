af6ba1322fe3a93e0afaf79b45550fe1c5108a7f
DaryaDudkova98 <dudkovadaryadmitrievna@mail.ru>
2026-09-03 00:25:25 +0300

    Fix vite-env.d.ts to resolve TS errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
-declare module '*.css' {
-  const content: Record<string, string>;
-  export default content;
-}
-
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
+/// <reference types="vite/client" />
